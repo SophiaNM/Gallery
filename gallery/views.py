563510325b9713e059f50677ecd88f77466bc971
SophiaNM <njerimurage92@outlook.com>
2018-07-14 12:53:03 +0300

    View functions for the index page

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,16 @@
-from django.http  import HttpResponse
+from django.http  import HttpResponse, Http404
+from django.shortcuts import render, redirect
+from .models import Image, Location, Category
+
 
 # Create your views here.
+
 def welcome(request):
-    return HttpResponse('Welcome ')
+    return render(request, 'index.html')
+
+
+
+def index(request):
+    images = Image.get_all_images()
+
+    return render(request, 'index.html', {"images": images})
