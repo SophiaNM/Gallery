53bc434790e1a8cae69fd18d80cbdef86f14d20b
SophiaNM <njerimurage92@outlook.com>
2018-07-14 12:53:26 +0300

    Setting up urls for view functions

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.index,name = 'index'),
+    # url(r'^search/', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
