f943da7f2e5864f81fb336e775be33a3ea5ae5f0
SophiaNM <njerimurage92@outlook.com>
2018-07-15 19:59:57 +0300

    Refactoring single template page

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^search/', views.search_image, name='search_image'),
     url(r'^location/(?P<image_location>\d+)', views.location_filter, name='location_filter'),
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single,name = 'single')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
