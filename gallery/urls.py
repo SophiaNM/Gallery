92377bf59351a2fa9bac659295bbeb3dcf2ad6b0
SophiaNM <njerimurage92@outlook.com>
2018-07-15 10:35:46 +0300

    Ading location filterand search image urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
-    # url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_image, name='search_image'),
+    url(r'^location/(?P<image_location>\d+)', views.location_filter, name='location_filter'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
